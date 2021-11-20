4840444cabb1f1fe8fa72ddc93f795b146a8bcc2
JimmyLv <jimmy.jinglv@gmail.com>
2021-11-20 12:39:54 +0800

    test: log error or warnings when happen

--- test/functional.test.js
+++ test/functional.test.js
@@ -3,5 +3,8 @@ import { runWebpackExampleInMemory } from './test-utils'
 test('should run with no errors or warnings', async () => {
   const buildStats = await runWebpackExampleInMemory('simple')
   const { errors, warnings } = buildStats
+  if ([...errors, ...warnings].length > 0) {
+    console.log({ errors, warnings })
+  }
   expect([...errors, ...warnings].length).toBe(0)
 })
