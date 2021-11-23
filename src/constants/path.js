c666c5b44befa048963b59591e3aecd7e02aa841
JimmyLv <jimmy.jinglv@gmail.com>
2021-11-23 20:40:10 +0800

    fix: update the cacheDir to dev/dll

--- src/constants/path.js
+++ src/constants/path.js
@@ -3,7 +3,7 @@ const getProjectRoot = require('../utils/getProjectRoot')
 const NODE_MODULES_PATH = path.resolve('./node_modules')
 
 const PROJECT_ROOT = getProjectRoot(NODE_MODULES_PATH)
-const DLL_ROOT = path.join(PROJECT_ROOT, '.atuodll-plugin')
+const DLL_ROOT = path.join(PROJECT_ROOT, 'dev', 'dll')
 const CACHE_HASH = path.join(DLL_ROOT, 'yarn.lock.md5')
 
 module.exports = { PROJECT_ROOT, DLL_ROOT, CACHE_HASH }
